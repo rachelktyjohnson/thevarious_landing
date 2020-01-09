302c97b0f43fedc7fb7aa5ec80ca0cff8409a073
Rachel Johnson <rachelktyjohnson@gmail.com>
2020-01-09 20:47:05 +1100

    :hammer: Hook up to database and add records

--- index.php
+++ index.php
@@ -1,3 +1,35 @@
+<?php
+
+$formMessage = "Let me know when The Various Directory launches!";
+
+$email =  filter_input(INPUT_POST,"your_email",FILTER_VALIDATE_EMAIL);
+if ($type==null){
+  $type="user";
+} else {
+  $type = filter_input(INPUT_POST,"type");
+}
+
+
+if (!empty($_POST)){
+  //if something has been posted
+  require('includes/connection.php');
+
+  try{
+    $sql = "INSERT INTO prelaunch_users(email,type) VALUES(?,?)";
+    $results = $db->prepare($sql);
+    $results->bindParam(1, $email, PDO::PARAM_STR);
+    $results->bindParam(2, $type, PDO::PARAM_STR);
+    $results->execute();
+
+    $formMessage = "Thanks for registering your interest! We'll let you know as soon as we're up and running!";
+  } catch (Exception $e) {
+      echo $e->getMessage();
+  }
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -13,10 +45,11 @@
     <main>
       <img class="main-image" src="img/image.png"/>
       <div class="email-signup">
-        <h2>Let me know when The Various Directory launches!</h2>
-        <form>
+        <h2><?= $formMessage?></h2>
+        <?php if (empty($_POST)){ ?>
+        <form method="POST">
           <div class="form-group checkbox-group">
-            <input type="checkbox" class="largerCheckbox" id="business" name="business" value="business">
+            <input type="checkbox" class="largerCheckbox" id="business" name="type" value="business">
             <label for="business">I am a business interested in being listed on The Various</label>
           </div>
           <div class="form-group form-group-email">
@@ -25,6 +58,7 @@
             <input class="submit-button" type="submit" value="GO">
           </div>
         </form>
+        <?php } ?>
       </div>
       <div class="social-media">
         <a href="https://www.facebook.com/thevarious.com.au/"><img src="img/facebook.svg"/></a>
